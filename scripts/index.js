9b5224e0b9c6499107aa1203497551f7206b8c54
Arex Jambusarwala <arexnj24@gmail.com>
2021-02-17 23:48:54 +0530

    Refactor index page

--- scripts/index.js
+++ scripts/index.js
@@ -8,16 +8,16 @@
     setTimeout(setCarouselAnimation, 13501);
 })();
 
-function expandSearch() {
+document.getElementById("search-bar").addEventListener("click", () => {
     document.getElementById("expanded-search-wrapper").style.display = "block";
     document.getElementById("search-bar-expanded").style.animation = "search 1.5s 0s 1 forwards";
     document.getElementById("close-search").style.animation = "close 1.5s 0s 1 forwards";
     document.getElementById("carousel").style.display = "none";
-    document.getElementById("flex-container").style.display = "none";
+    document.getElementById("button-container-wrapper").style.display = "none";
     setTimeout(() => {
         document.getElementById("search-bar-expanded").focus();
     }, 1300);
-}
+});
 
 document.getElementById("search-bar-expanded").addEventListener("focus", (event) => {
     document.getElementById("available-ingredients").style.display = "block";
@@ -25,7 +25,7 @@ document.getElementById("search-bar-expanded").addEventListener("focus", (event)
     document.getElementById("selected-ingredients").style.opacity = "0.3";
     document.getElementById("results").style.transition = "0.2s ease-in";
     document.getElementById("selected-ingredients").style.transition = "0.2s ease-in";
-    showOptions(event.target);
+    showOptions(event);
 });
 
 document.getElementById("search-bar-expanded").addEventListener("blur", () => {
@@ -35,17 +35,29 @@ document.getElementById("search-bar-expanded").addEventListener("blur", () => {
     document.getElementById("available-ingredients").style.display = "none";
 });
 
-function closeSearch() {
+document.getElementById("close-search").addEventListener("click", () => {
     document.getElementById("expanded-search-wrapper").style.display = "none";
     document.getElementById("carousel").style.display = "block";
-    document.getElementById("flex-container").style.display = "flex";
-}   
+    document.getElementById("button-container-wrapper").style.display = "flex";
+});
 
 let selectedIngredients  = [];
 
-function showOptions(target) {
+document.getElementById("search-bar-expanded").addEventListener("keyup", showOptions);
+
+function createButton({className, value, innerHTML, eventHandler}, event = "click") {
+    let btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.setAttribute("class", className);
+    btn.setAttribute("value", value);
+    btn.innerHTML = innerHTML;
+    btn.addEventListener(event, eventHandler);
+    return btn;
+}
+
+function showOptions(event) {
     document.getElementById("available-ingredients").innerHTML = "";
-    let curString = target.value.toLowerCase();
+    let curString = event.target.value.toLowerCase();
     console.log(curString);
     if(!curString)
         return;
@@ -53,12 +65,14 @@ function showOptions(target) {
         if(selectedIngredients.includes(ingredient))
             continue;
         if(ingredient.includes(curString)) {
-            let but = document.createElement("button");
-            but.setAttribute("class", "toggle-ingredient available");
-            but.setAttribute("value", ingredient);
-            but.innerHTML = ingredient;
-            but.addEventListener("mousedown", addToSelectedList);
-            document.getElementById("available-ingredients").appendChild(but);
+            document.getElementById("available-ingredients").appendChild(
+                createButton({
+                    className: "toggle-ingredient available", 
+                    value: ingredient, 
+                    innerHTML: ingredient, 
+                    eventHandler: addToSelectedList
+                }, "mousedown")
+            );
         }
     }
 }
@@ -66,15 +80,16 @@ function showOptions(target) {
 function addToSelectedList(event) {
     const value = event.target.value;
     event.target.remove();
-    if(selectedIngredients.indexOf(value) === -1)
-        selectedIngredients.push(value);
+    selectedIngredients.push(value);
     showRecipes();
-    let but = document.createElement("button");
-    but.setAttribute("class", "toggle-ingredient selected");
-    but.setAttribute("value", value);
-    but.addEventListener("click", removeFromSelectedList);
-    but.innerHTML = value;
-    document.getElementById("selected-ingredients").appendChild(but);
+    document.getElementById("selected-ingredients").appendChild(
+        createButton({
+            className: "toggle-ingredient selected", 
+            value: value, 
+            innerHTML: value, 
+            eventHandler: removeFromSelectedList
+        })
+    );
 }
 
 function removeFromSelectedList() {
@@ -82,46 +97,62 @@ function removeFromSelectedList() {
     showRecipes();
     if(document.getElementById("search-bar-expanded").value
         && (this.value).includes(document.getElementById("search-bar-expanded").value)) {
-        let but = document.createElement("button");
-        but.setAttribute("class", "toggle-ingredient available");
-        but.setAttribute("value", this.value);
-        but.innerHTML = this.value;
-        but.addEventListener("click", addToSelectedList);
-        document.getElementById("available-ingredients").appendChild(but);
+        document.getElementById("available-ingredients").appendChild(
+            createButton({
+                className: "toggle-ingredient available", 
+                value: this.value, 
+                innerHTML: this.value, 
+                eventHandler: addToSelectedList
+            })
+        );
     }
     this.remove();
 }
 
+function createItemImage(src) {
+    let itemImage = document.createElement("img");
+    itemImage.classList.add("result-item-image");
+    itemImage.src = src || defaultImage;
+    return itemImage;
+}
+
+function createItemName(name) {
+    let itemName = document.createElement("span");
+    itemName.classList.add("result-item-name");
+    itemName.innerHTML = name;
+    return itemName;
+}
+
+function createItemIngredients(ingredients) {
+    let itemIngredients = document.createElement("span");
+    itemIngredients.classList.add("result-item-ingredients");
+    itemIngredients.innerHTML = ingredients.reduce((acc, val, ind) => {
+        return ind !== ingredients.length -1 ? acc+=(val + ", ") : acc+=val;
+    },"");
+    return itemIngredients;
+}
+
+function createItem(recipe) {
+    let item = document.createElement("a"); 
+    item.classList.add("result-item");
+    item.href = "item.html";
+    let breakPoint = document.createElement("br");
+    item.appendChild(createItemImage(recipe.images[0]));
+    item.appendChild(breakPoint);
+    item.appendChild(createItemName(recipe.itemName));
+    item.appendChild(breakPoint);
+    item.appendChild(createItemIngredients(recipe.ingredients));
+    return item;
+}
+
 function showRecipes() {
     document.getElementById("results").innerHTML = "";
-    if(!selectedIngredients.length) {
+    if(!selectedIngredients.length)
         return;
-    }
     let recipesToBeDisplayed = recipes.filter(recipe => 
         selectedIngredients.every(ingredient => recipe.ingredients.includes(ingredient))
     );
-    //console.log(recipesToBeDisplayed);
     for(recipe of recipesToBeDisplayed) {
-        let item = document.createElement("a");
-        item.classList.add("result-item");
-        item.href = "item.html";
-        let itemImage = document.createElement("img");
-        itemImage.classList.add("result-item-image");
-        itemImage.src = recipe.images[0];
-        let breakPoint = document.createElement("br");
-        let itemName = document.createElement("span");
-        itemName.classList.add("result-item-name");
-        itemName.innerHTML = recipe.itemName;
-        let itemIngredients = document.createElement("span");
-        itemIngredients.classList.add("result-item-ingredients");
-        itemIngredients.innerHTML = recipe.ingredients.reduce((acc, val, ind) => {
-            return ind !== recipe.ingredients.length -1 ? acc+=(val + ", ") : acc+=val;
-        },"");
-        item.appendChild(itemImage);
-        item.appendChild(breakPoint);
-        item.appendChild(itemName);
-        item.appendChild(breakPoint);
-        item.appendChild(itemIngredients);
-        document.getElementById("results").appendChild(item);
+        document.getElementById("results").appendChild(createItem(recipe));
     }
 }
